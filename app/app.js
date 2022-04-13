093d95769298b11f78f570c449cfc47a89675a4e
Thiago de Sousa Pereira <53672557+thdesous@users.noreply.github.com>
2022-04-13 20:30:40 -0300

    Alteracao de estrutura do app

--- app/app.js
+++ app/app.js
@@ -1,9 +1,23 @@
-//app.js
-const express = require("express");
-const app = express();
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+function welcomeMessage(){
+    var message = "Hello World";
+    return message;
+}
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    // var message = "Hello World";
+    res.render("index", {message: welcomeMessage()});
 });
 
-module.exports = app;
+var server = app.listen(3000, function () {
+    console.log("Node server is running on http://localhost:3000");
+});
+
+module.exports = server;
+module.exports.welcomeMessage = welcomeMessage;
