85c98b49035c1120e557375450b23b445883c919
Thiago de Sousa Pereira <53672557+thdesous@users.noreply.github.com>
2022-04-13 16:31:34 -0300

    Alteracao de estrutura webapp

--- app/server.js
+++ app/server.js
@@ -1,16 +1,10 @@
-'use strict';
-
+//server.js
 const express = require('express');
 
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const app = require("./app");
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
